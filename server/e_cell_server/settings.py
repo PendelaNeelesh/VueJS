efe0d593a15670221e4a78fb2ef839e52d093bea
PendelaNeelesh <67135821+PendelaNeelesh@users.noreply.github.com>
2021-08-05 11:53:22 +0530

    Update settings.py

--- server/e_cell_server/settings.py
+++ server/e_cell_server/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,3 +136,5 @@ BASE_DIR.joinpath('static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
